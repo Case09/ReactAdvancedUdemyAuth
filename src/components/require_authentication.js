13d457cce789e4bd8a56c7e15847b6164f0cf77c
Case09 <filuzi77@gmail.com>
2017-06-28 10:57:56 +0200

    Added some explanation for auth HOC

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -9,3 +9,13 @@ export default function(ComposedComponent) {
 
   return Authentication;
 }
+
+// In some other location... Not in this file...
+// We want to use this HOC
+// import Authentication // This is my HOC
+// import Resources // This is the component i want to wrap
+
+// const ComposedComponent = Authentication(Resources);
+
+// // In some render method...
+// <ComposedComponent />
