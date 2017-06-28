f50bfca8c573218532e691ea3d259e55d67cb087
Case09 <filuzi77@gmail.com>
2017-06-28 11:47:21 +0200

    Added HOC authentication methods, added context to get access to router, final basic app done

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function(ComposedComponent) {
   class Authentication extends Component {
+    // contextTypes is used to get access to router inside out class
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
     render() {
       return <ComposedComponent {...this.props} />
     }
   }
 
-  return Authentication;
+  function mapStatetoProps(state) {
+    return {
+      authenticated: state.authenticated
+    };
+  }
+
+  return connect(mapStatetoProps)(Authentication);
 }
 
 // In some other location... Not in this file...
